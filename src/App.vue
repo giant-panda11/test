91813668b05d6781cffefc0257ff3d16049c2805
panda <panda>
2024-05-04 22:38:32 +0800

    提交格式化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const src = "fsfsdf"
+for(let i = 0;i < 2;i++) 
+{
+  console.log(i)
+}
+</script>
+
+<template>
+  <div class="a">fsd</div>
+</template>
+
+<style scoped>
+a 
+{
+  color: red;
+}
+</style>
